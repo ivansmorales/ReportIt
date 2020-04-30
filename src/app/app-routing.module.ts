af374baa2da2fd58002dcfc544cd6688d6bf15f8
Iván Morales <48416564+ivansmorales@users.noreply.github.com>
2020-04-29 21:12:13 -0500

    Just changed a word
    Changed component, for module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import {LandingPageComponent} from './main_components/landing-page/landing-page.
 import {PageNotFoundComponent} from './main_components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  //{path: 'users', loadChildren: './userModule/user-module.component#UserModuleModule'},
-  //{path: 'admins', loadChildren: './adminModule/admin-module.component#AdminModuleModule'},
+  //{path: 'users', loadChildren: './userModule/user-module.module#UserModuleModule'},
+  //{path: 'admins', loadChildren: './adminModule/admin-module.module#AdminModuleModule'},
     {path: '', component: LandingPageComponent},
     {path: '**', component: PageNotFoundComponent }
 ];
