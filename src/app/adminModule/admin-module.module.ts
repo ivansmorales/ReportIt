77db3ac456fbcc83ccacedb229450fdbeca7ffd2
DiegoMoreno <moreno.diego98@live.com.mx>
2020-04-30 22:48:15 -0500

    AdminRouting

--- src/app/adminModule/admin-module.module.ts
+++ src/app/adminModule/admin-module.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import {AdminRoutingModule} from './admin-routing.module';
+
 import { LoginAdminComponent } from './login-admin/login-admin.component';
 import { FormLoginComponent } from './login-admin/components/form-login/form-login.component';
 import { HeaderAdminComponent } from './login-admin/components/header-admin/header-admin.component';
@@ -10,7 +13,8 @@ import {ReportsAdminComponent} from './reports-admin/reports-admin.component';
 @NgModule({
   declarations: [LoginAdminComponent, FormLoginComponent, HeaderAdminComponent, ReportsAdminComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutingModule
   ]
 })
 export class AdminModuleModule { }
