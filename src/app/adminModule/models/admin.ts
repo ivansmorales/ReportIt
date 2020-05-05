022f38226254306b875c07d7fdaa6cdbe63b46df
Ivan Morales <ivann_mg@hotmail.com>
2020-05-04 21:17:24 -0500

    Completed class admin and service admin

--- src/app/adminModule/models/admin.ts
+++ src/app/adminModule/models/admin.ts
@@ -1,2 +1,10 @@
 export class Admin {
+
+    public correo? : string;
+    public contraseña? : string;
+
+    constructor(
+        public nombre? : string,
+        public id? : string
+    ){}
 }
--- src/app/adminModule/services/admin.service.ts
+++ src/app/adminModule/services/admin.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {Admin} from '../models/admin';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
+  admins = [new Admin("Administrador", "id") ];
+
   constructor() { }
+
+  getAdmins() : Admin[]{
+    return this.admins;
+  }
+
+  addAdmin(admin : Admin){
+    this.admins.push(admin);
+  }
+
+  deleteAdmin(admin : Admin){
+    this.admins.splice(1,0);
+  }
+
 }
