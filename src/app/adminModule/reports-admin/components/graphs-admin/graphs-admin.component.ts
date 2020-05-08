4a3f72d879af7b108bec32db84be5e19de7c2d48
Ivan Morales <ivann_mg@hotmail.com>
2020-05-07 20:40:01 -0500

    Added 2 extra graphics

--- src/app/adminModule/reports-admin/components/graphs-admin/graphs-admin.component.ts
+++ src/app/adminModule/reports-admin/components/graphs-admin/graphs-admin.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import * as CanvasJS from 'node_modules/canvasjs/dist/canvasjs.min.js';
-
+//import * as CanvasJS from 'node_modules/canvasjs/dist/canvasjs.min.js';
+import * as CanvasJS from 'src/assets/canvasjs.min.js';
 
 @Component({
   selector: 'app-graphs-admin',
@@ -14,9 +14,9 @@ export class GraphsAdminComponent implements OnInit {
 
 	ngOnInit() : void{
     
-    let chart = new CanvasJS.Chart("chartContainer", {
+    let chart1 = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
-        exportEnabled: true,
+        theme: 'light2',
         title: {
           text: "Estados con mayor cantidad de reportes en 2020"
         },
@@ -33,8 +33,46 @@ export class GraphsAdminComponent implements OnInit {
           ]
         }]
       });
-      
-    chart.render();
+    let chart2 = new CanvasJS.Chart("chartContainer2", {
+      theme: "light2",
+      animationEnabled: true,
+      title: {
+        text: "Reportes sobre accidentes de auto por mes durante el 2020 en la CDMX"
+      },
+      data: [{
+        type: "column",
+        dataPoints: [
+          { y: 8200, label: "Enero" },
+          { y: 9500, label: "Febrero" },
+          { y: 4300, label: "Marzo" },
+          { y: 943, label: "Abril" },
+          { y: 220, label: "Mayo" }
+        ]
+      }]
+    });
+    let chart3 = new CanvasJS.Chart("chartContainer3", {
+      theme: "light1",
+      animationEnabled: true,
+      title: {
+        text: "Tipos de reportes presentados en 2019 en todo México"
+      },
+      data: [{
+        type: "pie",
+        startAngle: 240,
+        indexLabel: "{label}",
+        dataPoints: [
+          { y: 30432, label: "Homicidio" },
+          { y: 14382, label: "Asaltos" },
+          { y: 16432, label: "Desapariciones" },
+          { y: 70323, label: "Acoso sexual" },
+          { y: 32933, label: "Autos mal estacionados" },
+          { y: 40345, label: "Accidentes de auto" }
+        ]
+      }]
+    });
+    chart1.render();
+    chart2.render();
+    chart3.render();
   }
 
 }
